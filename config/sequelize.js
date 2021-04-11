120a976c50be8c4d6c4af711ad7db9737704ae31
Kavin valli <kavinvalli@gmail.com>
2021-04-11 16:42:18 +0530

    allow ssl for postgres

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,5 +1,7 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  native: true,
+});
 
 module.exports = sequelize;
