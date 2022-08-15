ec4171609b5913a95e6d80d6c480ec1b2586e139
AyonC <e10901ayon@dpsrkp.net>
2022-08-15 15:50:41 +0530

    Fix glitch

--- events/message.js
+++ events/message.js
@@ -1,23 +1,23 @@
-const { client } = require("../index.js")
+const { client } = require("../index.js");
 module.exports = {
     name: "message",
     once: false,
     execute: async (message) => {
-        prefix = "t!" || "t~"
+        prefix = "t!" || "t~";
 
         const args = msg.startsWith(prefix)
             ? msg.slice(prefix.prefix).trim().split(/ +/g)
-            : [""]
+            : [""];
         // const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
-        const command = args.shift().toLowerCase()
+        const command = args.shift().toLowerCase();
 
-        if (!client.commands.has(command)) return
+        if (!client.commands.has(command)) return;
 
         try {
-            client.commands.get(command).run(client, message, args)
+            client.commands.get(command).run(client, message, args);
         } catch (error) {
-            console.error(error)
+            console.error(error);
         }
     },
-}
+};
