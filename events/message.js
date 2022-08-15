358e491faa39517bacec91f899a132ddd3f2e5ca
AyonC123 <106944474+AyonC123@users.noreply.github.com>
2022-08-15 16:01:35 +0530

    Remove prettier changes manually oooof

--- events/message.js
+++ events/message.js
@@ -1,23 +1,21 @@
 const { client } = require("../index.js");
 module.exports = {
-    name: "message",
-    once: false,
-    execute: async (message) => {
-        prefix = "t!" || "t~";
+  name: "message",
+  once: false,
+  execute: async (message) => {
+    prefix = "t!" || "t~";
 
-        const args = msg.startsWith(prefix)
-            ? msg.slice(prefix.prefix).trim().split(/ +/g)
-            : [""];
-        // const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = msg.startsWith(prefix) ? msg.slice(prefix.prefix).trim().split(/ +/g) : [""];
+    // const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
-        const command = args.shift().toLowerCase();
+    const command = args.shift().toLowerCase();
 
-        if (!client.commands.has(command)) return;
+    if (!client.commands.has(command)) return;
 
-        try {
-            client.commands.get(command).run(client, message, args);
-        } catch (error) {
-            console.error(error);
-        }
-    },
+    try {
+        client.commands.get(command).run(client, message, args);
+    } catch (error) {
+        console.error(error);
+    }
+  },
 };
